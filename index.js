a41da73ceebe9790f01dee240f57eab487709bcf
avtzone <avtzone1@gmail.com>
2025-09-29 15:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
 const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Body parser first
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// Import routes
+let server = require('./qr');
+let code = require('./pair');
+
+// Routes
+app.use('/qr', server);
+app.use('/code', code);
+
+app.use('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
